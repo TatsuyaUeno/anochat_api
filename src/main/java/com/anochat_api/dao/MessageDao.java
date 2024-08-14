916d31a33a51660626c37b4a0dca1724702d5f69
ICD-CORP\m-komatsu <m-komatsu@icd.co.jp>
2024-08-14 18:36:39 +0900

    メッセージ一覧取得API動作確認コード追加

--- src/main/java/com/anochat_api/dao/MessageDao.java
+++ src/main/java/com/anochat_api/dao/MessageDao.java
@@ -1,5 +1,6 @@
 package com.anochat_api.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,22 @@ public class MessageDao {
     @Autowired
     private MessageMapper messageMapper;
 
-    // @Transactional
+    // @Transactional 更新・削除APIでは必要らしい
     // public List<MessageDto> findMessageList(int chatid) {
     //     return messageMapper.getMessageList(chatid);
     // }
 
     // @Transactional
     public List<MessageDto> getMessage(int chatid) {
+
+        // DBなし動作確認用
+        // List<MessageDto> result = new ArrayList<>();
+        // MessageDto data1 = new MessageDto(101, 1, "message");
+        // MessageDto data2 = new MessageDto(101, 2, "testmsg");
+        // result.add(data1);
+        // result.add(data2);
+        // return result;
+
         return messageMapper.getMessagesByChatId(chatid);
     }
 }
